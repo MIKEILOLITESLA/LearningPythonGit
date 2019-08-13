ec9ce838a0ea4a581d99f71171a633063e1d1315
zyc <zhangyichi1997@gmail.com>
2019-08-13 19:03:43 +0800

    the code typed between 5th Aug and 11th Aug

---
+++ 第一周py练习文件/variables and data types.py
@@ -0,0 +1,36 @@
+#variables and data types
+variable_1 = 20
+variable_2 = 20
+variable_2 += 10
+variable_1 *= 4
+print(variable_1)
+print(variable_2)
+#
+variable_1 = 10
+variable_2 =2.5
+variable_1 += 6.5
+variable_2 *= 2
+print(variable_1)
+print(variable_2)
+#
+variable_a = 13.9
+variable_b = 2.8
+variable_a = round(variable_a)
+variable_b = int(variable_b)
+print(variable_a)
+print(variable_b)
+#
+app_name = "Pandora - Music & Radio"
+average_rating = "4.0"
+total_ratings = "1724546"
+price = "free"
+print(app_name)
+#
+motto = '''Facebook's new motto is "move fast with stable infra."'''
+print(motto)
+#
+facebook = "Facebook's rating is"
+fb_rating = 3.5
+fb_rating_str = str(fb_rating)
+fb = facebook + fb_rating_str
+print(fb)
