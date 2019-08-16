e47d4c934034a9d4a07ef1cbf64224ccd745057f
zyc <zhangyichi1997@gmail.com>
2019-08-16 23:13:33 +0800

    16th coding

--- 第三周练习py文件/variables_and_data_types.py
+++ 第三周练习py文件/variables_and_data_types.py
@@ -1,10 +1,13 @@
-#variables变量
+#variables_and_data_types
+
+# variables变量
 variable_1 = 20
 variable_2 = 20
 variable_2 += 10
 variable_1 *= 4
 print(variable_1)
 print(variable_2)
+
 #variables变量
 variable_1 = 10
 variable_2 =2.5
@@ -12,6 +15,7 @@ variable_1 += 6.5
 variable_2 *= 2
 print(variable_1)
 print(variable_2)
+
 #variables & int变量与整数类型
 variable_a = 13.9
 variable_b = 2.8
@@ -19,19 +23,23 @@ variable_a = round(variable_a)
 variable_b = int(variable_b)
 print(variable_a)
 print(variable_b)
+
 #data types: str字符串
 app_name = "Pandora - Music & Radio"
 average_rating = "4.0"
 total_ratings = "1724546"
 price = "free"
 print(app_name)
+
 #data types: str字符串
 motto = '''Facebook's new motto is "move fast with stable infra."'''
 print(motto)
+
 #data types: str & float字符串与浮点
 facebook = "Facebook's rating is"
 fb_rating = 3.5
 fb_rating_str = str(fb_rating)
 fb = facebook + fb_rating_str
 print(fb)
+
 #这一块主要是一些数据类型和变量的小练习，数据类型已经认识了。
